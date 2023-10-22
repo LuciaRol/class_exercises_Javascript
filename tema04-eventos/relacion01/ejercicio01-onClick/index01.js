7b94c9e25f9b01d66656530611536bfbb072382b
Lucia <luciarol87@gmail.com>
2023-10-22 13:00:26 +0200

    ejercicio 1, worksheet 1 del tema 4

---
+++ tema04-eventos/relacion01/ejercicio01-onClick/index01.js
@@ -0,0 +1,17 @@
+const boton = document.getElementById("boton");
+
+
+/**
+boton.onclick = function (){
+    alert("Funciona!!");
+}
+ */
+
+
+ boton.onclick = () => {
+    alert("Funciona!!");
+}
+
+
+
+    
