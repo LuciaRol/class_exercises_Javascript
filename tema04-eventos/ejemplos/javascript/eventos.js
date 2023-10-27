a1525ea93dd0c57afbe25d913988930de950d172
Lucia <luciarol87@gmail.com>
2023-10-27 12:45:38 +0200

    Ejercicio 1 de la relación 5.
    Funciones para validar formularios, sin terminar.

--- tema04-eventos/ejemplos/javascript/eventos.js
+++ tema04-eventos/ejemplos/javascript/eventos.js
@@ -2,6 +2,6 @@ const btn = document.getElementById("btn")
 
 btn.addEventListener("click", () => {
     document.body.style.backgroundColor = "red";
-    connsole.log("Salta evento");
+    console.log("Salta evento");
 }
 )
