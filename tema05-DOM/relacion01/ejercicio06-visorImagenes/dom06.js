f2709b80d2aa26315c99185720458d6ca55755e5
Lucia <luciarol87@gmail.com>
2023-11-02 20:30:24 +0100

    Ejercicio 6 de la relacion 1 del tema 5 (no funciona).

---
+++ tema05-DOM/relacion01/ejercicio06-visorImagenes/dom06.js
@@ -0,0 +1,31 @@
+window.onload = ()=> {
+
+    pikachu = document.getElementById("img");
+    anteriorBtn = document.getElementById("anteriorBtn");
+    siguienteBtn = document.getElementById("siguienteBtn")
+    pikachuActual = 1;
+    imagenesPikachu = [];
+  
+    mostrarPikachu = (seleccionImagenes) => {
+      if (seleccionImagenes >= 1 && seleccionImagenes <= imagenesPikachu) {
+        pikachu.src = "img" + seleccionImagenes + ".png";
+        pikachu.alt = "img " + seleccionImagenes;
+        pikachuActual = seleccionImagenes;
+      }
+    }
+  
+    anteriorBtn.addEventListener("click",() => {
+      if (pikachuActual > 1) {
+        mostrarPikachu(pikachuActual - 1);
+      }
+    });
+  
+    siguienteBtn.addEventListener("click",() => {
+      if (pikachuActual < imagenesPikachu) {
+        mostrarPikachu(pikachuActual + 1);
+      }
+    });
+  
+    mostrarPikachu(pikachuActual);
+  };
+  
